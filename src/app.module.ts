afdb967a353f72d917d07c301ab43523508911f3
stacy1930 <35913788+stacy1930@users.noreply.github.com>
2019-03-27 10:09:05 +0100

    creat uilisateur

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UtilisateurController } from './utilisateur/utilisateur.controller';
+import { UtilisateurModule } from './utilisateur/utilisateur.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UtilisateurModule],
+  controllers: [AppController, UtilisateurController],
   providers: [AppService],
 })
 export class AppModule {}
