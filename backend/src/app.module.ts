fa5bca7af518167f5b965dac1a0421d1bc461421
stacy1930 <35913788+stacy1930@users.noreply.github.com>
2019-03-27 15:40:31 +0100

    envoie

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,15 +11,12 @@ import { VilleModule } from './ville/ville.module';
 import { UtilisateurController } from './utilisateur/utilisateur.controller';
 import { DependanceController } from './dependance/dependance.controller';
 import { ImageController } from './image/image.controller';
-<<<<<<< HEAD
 import { DepartementController } from './departement/departement.controller';
 import { AchatController } from './achat/achat.controller';
 import { AchatModule } from './achat/achat.module';
-=======
 import { MdpController } from './mdp/mdp.controller';
 import { DepartementModule } from './departement/departement.module';
 import { MdpModule } from './mdp/mdp.module';
->>>>>>> 1e3273a2d277ac21e0856129cbb213ca2fd8084b
 
 @Module({
   imports: [
@@ -30,23 +27,17 @@ import { MdpModule } from './mdp/mdp.module';
     ImageModule,
     UtilisateurModule,
     VilleModule,
-<<<<<<< HEAD
     AchatModule,
-=======
     DepartementModule,
     MdpModule,
->>>>>>> 1e3273a2d277ac21e0856129cbb213ca2fd8084b
   ],
   controllers: [
     AppController,
     UtilisateurController,
     DependanceController,
     ImageController,
-<<<<<<< HEAD
     AchatController,
-=======
     MdpController,
->>>>>>> 1e3273a2d277ac21e0856129cbb213ca2fd8084b
   ],
   providers: [AppService],
 })
