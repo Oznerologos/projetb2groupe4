9fd1a10f452f9f5b2c8cf51f51bfa11fce4244d0
AntoineAcquart <antoine.acquart@ynov.com>
2019-03-27 10:36:20 +0100

    ajout ormconfig et modifs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UtilisateurController } from './utilisateur/utilisateur.controller';
 import { UtilisateurModule } from './utilisateur/utilisateur.module';
+import { AgenceModule } from './agence/agence.module';
 
 @Module({
-  imports: [UtilisateurModule],
+  imports: [TypeOrmModule.forRoot(), UtilisateurModule, AgenceModule],
   controllers: [AppController, UtilisateurController],
   providers: [AppService],
 })
