5048ae3584df28b9e17deb64f05bc42a06c264c3
stacy1930 <35913788+stacy1930@users.noreply.github.com>
2019-03-27 12:05:07 +0100

    image

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,12 @@ import { UtilisateurController } from './utilisateur/utilisateur.controller';
 import { UtilisateurModule } from './utilisateur/utilisateur.module';
 import { AgenceModule } from './agence/agence.module';
 import { DependanceController } from './dependance/dependance.controller';
+import { ImageController } from './image/image.controller';
+import { ImageModule } from './image/image.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UtilisateurModule, AgenceModule],
-  controllers: [AppController, UtilisateurController, DependanceController],
+  imports: [TypeOrmModule.forRoot(), UtilisateurModule, AgenceModule, ImageModule],
+  controllers: [AppController, UtilisateurController, DependanceController, ImageController],
   providers: [AppService],
 })
 export class AppModule {}
