45ea8403c7fb324d48b52a1c878bde8851f8cc0c
Oznerologos <lorenzo.logos@ynov.com>
2019-05-15 14:41:56 +0200

    reset propre

--- src/utilisateur/utilisateur.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UtilisateurService } from './utilisateur.service';
-
-@Module({
-  providers: [UtilisateurService]
-})
-export class UtilisateurModule {}
